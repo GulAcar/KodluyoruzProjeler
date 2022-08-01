e927372240106fd26e0a769f4397fb0d6ad93ab4
GulAcar <gulacar326@gmail.com>
2022-08-01 17:53:02 +0300

    new project

---
+++ faktriyl/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        int top=1,n;
+        System.out.print("Sayi giriniz:");
+        n=input.nextInt();
+
+        for (int i=1;i<=n;i++){
+
+            top=top*i;
+            System.out.println("sayının faktorıyelı:"+top);
+
+
+        }
+
+
+
+
+
+
+
+
+    /*
+
+     */
+
+    }
+}
